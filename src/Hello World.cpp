14f59ee38245e11368aa23aa59c13b17f97f0907
telewide <smartdome@gmail.com>
2018-04-04 20:02:48 +0900

    create project

---
+++ src/Hello World.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : telewide<smartdome@gmail.com>
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
