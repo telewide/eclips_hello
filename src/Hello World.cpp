cedd09d91bb98f3517480f62b07afa8eaf95ce1d
telewide <smartdome@gmail.com>
2018-04-05 22:30:54 +0900

    del Print line

--- src/Hello World.cpp
+++ src/Hello World.cpp
@@ -11,6 +11,5 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
