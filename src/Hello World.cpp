2755703cb9b99d00289ddfe0b2642a9a1fa2bb8c
telewide <smartdome@gmail.com>
2018-04-06 21:52:45 +0900

    Local git Print line

--- src/Hello World.cpp
+++ src/Hello World.cpp
@@ -11,5 +11,6 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Local git add line Print!!!" << endl; // prints Local git add line Print!!!
 	return 0;
 }
